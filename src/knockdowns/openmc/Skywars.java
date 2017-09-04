d5abda6a19570bcc5e6d44f9c82bdfb8146b9456
Knockdowns <rpekkala27@gmail.com>
2017-09-03 20:59:54 -0400

    Fixed code not running
    Fixed code not running

--- src/knockdowns/openmc/Skywars.java
+++ src/knockdowns/openmc/Skywars.java
@@ -1,9 +1,11 @@
 package knockdowns.openmc;
 
+import knockdowns.openmc.listeners.PlayerListeners;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
@@ -32,7 +34,7 @@ public class Skywars extends JavaPlugin
     @Override
     public void onDisable() {
         saveConfig();
-        
+
     }
 
     private void registerCommands() {
@@ -40,6 +42,7 @@ public class Skywars extends JavaPlugin
     }
 
     private void registerListeners() {
-
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new PlayerListeners(this), this);
     }
 }
